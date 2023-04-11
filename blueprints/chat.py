8f63a598767f88925e1e9d41d4151fd529f522e0
bljboy <bljboy55@gmail.com>
2023-04-12 04:42:04 +0800

    用户一对一聊天功能完成

--- blueprints/chat.py
+++ blueprints/chat.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, json, request, jsonify
 from flask_socketio import join_room, emit
 
 from exts import mail, db, socketio
@@ -6,23 +6,42 @@ from exts import mail, db, socketio
 bp = Blueprint('chat', __name__, url_prefix='')
 
 
-@bp.route('/')
-def index():
-    print("chat")
-    return "chat"
-
-
 @socketio.on('connect')
 def on_connect():
     print('客户端已连接')
-    join_room('chatroom')
 
 
 @socketio.on('message')
 def handle_message(data):
-    print('收到消息：', data)
+    # 发送消息到特定房间
+    print(data)
     sender_id = data['sender_id']
     receiver_id = data['receiver_id']
     content = data['content']
-    emit('message', {'sender_id': sender_id, 'receiver_id': receiver_id, 'content': content}, room='chatroom')
+    room = get_room_id(sender_id, receiver_id)
+    join_room(room)
+    emit('message', {'sender_id': sender_id, 'receiver_id': receiver_id, 'content': content}, room=room)
     return content
+
+
+def get_room_id(sender_id, receiver_id):
+    # 将两个用户名按照字典序排序
+    sorted_names = sorted([sender_id, receiver_id])
+    user1_name = sorted_names[0]
+    user2_name = sorted_names[1]
+    # 根据用户名创建唯一的房间 ID
+    return 'room_{}_{}'.format(user1_name, user2_name)
+# @socketio.on('connect')
+# def on_connect():
+#     print('客户端已连接')
+#     join_room('chatroom')
+#
+#
+# @socketio.on('message')
+# def handle_message(data):
+#     print('收到消息：', data)
+#     sender_id = data['sender_id']
+#     receiver_id = data['receiver_id']
+#     content = data['content']
+#     emit('message', {'sender_id': sender_id, 'receiver_id': receiver_id, 'content': content}, room='chatroom')
+#     return content
